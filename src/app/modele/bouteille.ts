fee9ec9cae14bc84c4a0acbd16e52ff093591b9c
damon1979 <portosbis@yahoo.fr>
2019-05-16 10:47:51 +0200

    Ajout de la propriété dateConso, à l'interface Bouteille.

--- src/app/modele/bouteille.ts
+++ src/app/modele/bouteille.ts
@@ -6,6 +6,7 @@ export interface Bouteille {
     couleur?: string;
     millesime?: number;
     garde?: number;
+    dateConso?: number
     source?: string;
     degustation?: boolean;
     commentaire?: string;
