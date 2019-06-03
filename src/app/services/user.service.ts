ef28f25461241c86c156182f9348d1ac0977f9cc
damon1979 <portosbis@yahoo.fr>
2019-06-03 14:46:55 +0200

    Typage explicite de la propriété "user".

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,29 +2,29 @@ import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/app';
 import { AngularFireAuth } from '@angular/fire/auth';
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UserService {
     isAuth: boolean;
-    public user;
+    public user: firebase.User;
     constructor(public afAuth: AngularFireAuth) {
-	this.isAuth = false;
-	firebase.auth().onAuthStateChanged((user) => {
-	    if (user) {
-		this.isAuth = true;
-		this.user = user;
-	    }
-	    else {
-		this.user = null;
-		this.isAuth = false;
-	    }
-	});
+        this.isAuth = false;
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.isAuth = true;
+                this.user = user;
+            }
+            else {
+                this.user = null;
+                this.isAuth = false;
+            }
+        });
     }
     login(email: string, pwd: string) {
-	return this.afAuth.auth.signInWithEmailAndPassword(email, pwd);
+        return this.afAuth.auth.signInWithEmailAndPassword(email, pwd);
     }
-    
+
     logout() {
-	return firebase.auth().signOut();
+        return firebase.auth().signOut();
     }
 }
